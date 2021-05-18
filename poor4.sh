b8740d4e93f04baff957b2db0c619a3be9f3e6a1
hioklnk <68479695+hioklnk@users.noreply.github.com>
2021-05-18 11:25:18 +0700

    Create poor4.sh

---
+++ poor4.sh
@@ -0,0 +1,4 @@
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RGnMXX3ohdnmQSCXarMgvrHSLYZdFnTSzv.adv5 -p x --cpu 4
